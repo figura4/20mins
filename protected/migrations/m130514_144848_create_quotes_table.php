2e3ca74752fdfa26eddd239bcad3fe84cdb632c8
oscar.riva <sezione9@gmail.com>
2013-05-14 16:52:44 +0200

    added quotes migration and model

---
+++ protected/migrations/m130514_144848_create_quotes_table.php
@@ -0,0 +1,22 @@
+<?php
+
+class m130514_144848_create_quotes_table extends CDbMigration
+{
+	public function safeUp()
+	{
+		$this->createTable('tbl_quote', array(
+				'id' 				=> 'pk',
+				'body'				=> 'text NOT NULL',
+				'source'			=> 'VARCHAR(200)',
+				'content_id'		=> 'int NOT NULL',
+				'random'			=> 'bool NOT NULL',
+				'created_on' 		=> 'DATETIME NOT NULL',
+				'updated_on' 		=> 'DATETIME NOT NULL',
+		), 'ENGINE=InnoDB CHARSET=utf8');
+	}
+
+	public function safeDown()
+	{
+		$this->dropTable('tbl_quote');
+	}
+}
