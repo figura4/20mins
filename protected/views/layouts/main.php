55e029309d9990425a17e3b324347abcb733dfe8
oscar.riva <sezione9@gmail.com>
2013-05-22 15:35:01 +0200

    completed main nav menu

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -32,6 +32,12 @@
 				array('label'=>'Home', 'url'=>array('/site/index')),
 				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
 				array('label'=>'Contact', 'url'=>array('/site/contact')),
+				array('label'=>'Contents', 'url'=>array('/content/admin'), 'visible'=>!Yii::app()->user->isGuest),
+				array('label'=>'Reviews', 'url'=>array('/review/admin'), 'visible'=>!Yii::app()->user->isGuest),
+				array('label'=>'Authors', 'url'=>array('/author/admin'), 'visible'=>!Yii::app()->user->isGuest),
+				array('label'=>'Quotes', 'url'=>array('/quote/admin'), 'visible'=>!Yii::app()->user->isGuest),
+				array('label'=>'Tags', 'url'=>array('/tag/admin'), 'visible'=>!Yii::app()->user->isGuest),
+				array('label'=>'Users', 'url'=>array('/user/admin'), 'visible'=>!Yii::app()->user->isGuest),
 				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
 				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
 			),
