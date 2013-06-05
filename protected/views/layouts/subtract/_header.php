6144216017169d8979057011f8f0601810809680
oscar.riva <sezione9@gmail.com>
2013-06-05 14:16:08 +0200

    cmenu widget

--- protected/views/layouts/subtract/_header.php
+++ protected/views/layouts/subtract/_header.php
@@ -5,12 +5,6 @@
 						<a href="#">20mins</a>
 						<h1 id="title">20 minutes into the future...</h1>
 					</div>
-					<!-- 
-					<ul class="nav-bar right" id="nav-bar">
-						<li class="has-flyout active"><a href="index.html">Home</a>
-						<li class="has-flyout"><a href="index.html">About</a>
-						<li class="has-flyout"><a href="index.html">Contact</a>
-					</ul> -->
 					<?php $this->widget('zii.widgets.CMenu',array(
 							'id'=>'nav-bar',
 							'items'=>array(
