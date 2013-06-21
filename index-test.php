efcf7ab5fb9842241f7a1f420c19a8279136f033
oscar.riva <sezione9@gmail.com>
2013-06-21 17:20:13 +0200

    layotu wrong after moving framework files

--- index-test.php
+++ index-test.php
@@ -5,7 +5,7 @@
  */
 
 // change the following paths if necessary
-$yii=dirname(__FILE__).'/../yii_framework/yii.php';
+$yii=dirname(__FILE__).'/protected/yii_framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/test.php';
 
 // remove the following line when in production mode
