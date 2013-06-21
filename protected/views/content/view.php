2abce03c52ceb6caf52da5343d7883ac89487660
oscar.riva <sezione9@gmail.com>
2013-06-21 16:53:50 +0200

    added partial folder

--- protected/views/content/view.php
+++ protected/views/content/view.php
@@ -34,33 +34,7 @@
 					<div class="clearfix"></div>
 				</div>
 				
-				<!--Comments-->
-				<div id="comments">
-					<h5><?php echo count($model->comments); ?> Commenti</h5>
-					<ul class="comments">
-						<?php foreach ($model->comments as $comment) { ?>
-							<li class="comment-cont">
-								<div class="avatar"><img src="/20mins/images/subtract/avatar.png" alt="" /></div>
-								<div class="comment">
-									<h6><?php echo $comment->author; ?></h6>
-									<p><?php echo $comment->body; ?></p>
-									<div class="date">at <?php echo date('G:ma', strtotime($model->created_on)) ?> on <?php echo date('F jS, Y', strtotime($model->created_on)) ?></div>
-									<!-- <a href="#" class="button right">Reply</a>  -->
-								</div>
-							</li>
-						<?php } ?>
-					</ul>
-				</div>
-				<!-- End Comments -->
-				
-				<!-- Form -->
-				<?php if(Yii::app()->user->hasFlash('commentSubmitted')): ?>
-        			<div class="comment-form">
-            			<h5><?php echo Yii::app()->user->getFlash('commentSubmitted'); ?></h5>
-        			</div>
-    			<?php else: ?>
-					<?php echo $this->renderPartial('_commentForm', array('comment'=>$comment)); ?>
-				<?php endif; ?>
-				<!-- End Form -->
+				<!--Comments section-->
+				<?php echo $this->renderPartial('../partials/_comments', array('model'=>$model, 'comment'=>$comment)); ?>
 
 			</div>
