f726c45e7540552333de8884f7d56d0e9b7f4390
oscar.riva <sezione9@gmail.com>
2013-06-27 16:18:20 +0200

    working on view review

--- protected/views/layouts/subtract/_sidebar.php
+++ protected/views/layouts/subtract/_sidebar.php
@@ -23,26 +23,5 @@
 						<a class="twitter-timeline" href="https://twitter.com/figura_4" data-widget-id="349907951680618498">Tweets di @figura_4</a>
 						<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 					</div>
-
-					<div class="widget">
-						<h5>Featured Post</h5>
-						<div class="featured-post">
-							<img src="img/nypd-featured.jpg" alt="" />
-							<h5><a href="#">This is a blog post!</a></h5>
-							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ultricies, diam...</p>
-							<div class="clearfix"></div>
-						</div>
-						<div class="featured-post-date">
-							January 29th, 2033
-						</div>
-					</div>
-					
-					<div class="widget">
-						<h5>Text Widget</h5>
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc at lacinia purus. Donec ac mauris et lacus pulvinar pulvinar eu in dui. Cras lacinia ultricies elit, commodo fermentum lacus euismod eget.</p>
-					</div>
-					
-				
-					
 				</div>
 			</aside>
